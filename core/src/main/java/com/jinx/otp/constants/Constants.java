92d0c8fa01d617aeead6f758b9ad0f9c3afcd573
Jinx OTP <erler.eliza@gmail.com>
2026-02-08 00:40:17 +0100

    Moved some constants to dedicated class.

---
+++ core/src/main/java/com/jinx/otp/constants/Constants.java
@@ -0,0 +1,10 @@
+package com.jinx.otp.constants;
+
+public class Constants {
+    public static float PLAYER_SPEED = 1f;
+    public static final float PLAYER_HEIGHT = 1f;
+    public static final float PLAYER_WIDTH = 0.5f;
+    
+    public static final String PLAYER_IMAGE_FILE_NAME = "player.png";
+
+}
---
+++ core/src/main/java/com/jinx/otp/constants/Direction.java
@@ -0,0 +1,8 @@
+package com.jinx.otp.constants;
+
+public enum Direction {
+    UP,
+    DOWN,
+    LEFT,
+    RIGHT
+}
