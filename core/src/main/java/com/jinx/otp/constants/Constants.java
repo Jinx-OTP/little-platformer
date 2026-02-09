e1c3947c61e804276447073bf651a632dbb05558
Jinx OTP <erler.eliza@gmail.com>
2026-02-09 13:37:31 +0100

    Better config for user movement. #2

--- core/src/main/java/com/jinx/otp/constants/Constants.java
+++ core/src/main/java/com/jinx/otp/constants/Constants.java
@@ -5,15 +5,15 @@ import com.badlogic.gdx.Application;
 public class Constants {
     public static int LOG_LEVEL = Application.LOG_DEBUG;
 
-    public static float PLAYER_SPEED = 1f;
+    public static float PLAYER_SPEED = 3f;
     public static final float PLAYER_HEIGHT = 1f;
     public static final float PLAYER_WIDTH = 0.5f;
-    public static final float PLAYER_JUMP_VELOCITY = 10f;
+    public static final float PLAYER_JUMP_VELOCITY = 7f;
     
     public static final String PLAYER_IMAGE_FILE_NAME = "player.png";
     public static final String PLATFORM_TEXTURE_PATH = "platform.png";
 
     public static final float DEFAULT_CAMERA_WIDTH = 10f;
     
-    public static final float GRAVITATION_CONSTANT = 5f;
+    public static final float GRAVITATION_CONSTANT = 9.81f;
 }
